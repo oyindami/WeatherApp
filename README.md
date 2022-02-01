aa6c2b3bdd57dd2bcf3e8f22308abdb9a500616f
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-01-31 22:11:23 -0500

    update file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # WeatherApp
+
+This appliaction is used to reques information from an API source adn post onto our hosted page
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Description
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Contact me
+
+[Github] (https://github.com/oyindami)
--- script.js
+++ script.js
@@ -1,33 +1,118 @@
-let Entercity= $("#Entercity");
-let searchcitybutton = $("#searchcitybutton");
 ///API keys from open weather source//
-const apiKey = "1e41fe8715bf0efe92d1f293a98c2c2d"
-let url = 'https://api.openweathermap.org/data/2.5/'
+let apiKey = "1e41fe8715bf0efe92d1f293a98c2c2d"; // oyins API key
+let url = "https://api.openweathermap.org/data/2.5/";
 
-function(addEventListener)
-addEventListener.searchcitybutton
+let searchbtn = $(".searchBTN");
+let searchBarEL = $("#searchBAR");
+let newButtonsEL = $(".newButtons");
+let currentDateEL = $(".currentDate");
+let nameDEL = $(".nameDate");
+let tempEL = $(".tempCD");
+let windEL = $(".windCD");
+let humidtyEL = $(".humidityCD");
+let VCDEL = $(".uVCD");
+let uVIndex = 0.0;
+let iconIMGDIVEL = $(".weatherICONCD");
+let newCityBtn = $(".newCity");
+var recentS = [];
 
-cityButtonSearch.on("click", inputCity);
-citySearch.on("keyup", function(event) {
-    if (event.keyCode === 13) {
-     event.preventDefault();
-     inputCity();
-
-
-const city = document. getElementById $("#Entercity")
-
-
-fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": "e720ccdb46msh86e5a226ae94a72p18652djsn86a91febdee3"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-	alert ("this works!")
+//SEARCH BUTTON
+searchbtn.on("click", function (event) {
+  event.preventDefault();
+  cityNameP = searchBarEL.val().trim(); //Get value
+  recentS.push(cityNameP); //update array
+  localStorage.setItem("cityNames", JSON.stringify(recentS)); //store in local
+  newButtonsEL.append(
+    '<button class="btn btn-primary" type="button">' + cityNameP + "</button>"
+  );
 });
+
+function display() {
+  var pullData = JSON.parse(localStorage.getItem("cityNames"));
+  if (pullData != null) {
+    for (var i = 0; i < pullData.length; i++) {
+      newButtonsEL.append(
+        '<button class="btn btn-primary" type="button"' +
+          "id=" +
+          '"' +
+          pullData[i] +
+          '"' +
+          ">" +
+          pullData[i] +
+          "</button>"
+      );
+    }
+  }
+  cityNameP = "Nigeria"; //Default
+  callAPI();
+}
+display();
+
+newButtonsEL.on("click", function (event) {
+  event.preventDefault();
+  console.log(event.target);
+  cityNameP = event.target.id;
+  console.log(cityNameP);
+  fiveCardsEL.children().remove(); //clears the 5 cards for new days!
+  iconIMGDIVEL.children().remove(); //clears images
+  callAPI();
+});
+
+//Calling from the open weather app API
+function callAPI() {
+  var mainURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityNameP +
+    "&units=imperial&appid=1e41fe8715bf0efe92d1f293a98c2c2d";
+  fetch(mainURL) //GET is the default.
+    .then(function (response) {
+      //GET responses
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var iconE = data.weather[0].icon;
+      var urlICON = "http://openweathermap.org/img/wn/" + iconE + "@2x.png";
+      iconIMGDIVEL
+        .append('<img alt = "Weather Icon" class="weatherIconI">')
+        .attr("src", urlICON);
+      var imgICON = $(".weatherIconI");
+      imgICON.attr("src", urlICON);
+
+      var nameDate =
+        data.name + " (" + moment.unix(data.dt).format("MMM Do, YYYY") + ")";
+      var temp = data.main.temp + " °F";
+      var wind = data.wind.speed + "MPH";
+      var humidity = data.main.humidity + "%";
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      uv(lat, lon);
+
+      //card information is pasted
+      nameDEL.text(nameDate);
+      tempEL.text("Temperature: " + temp);
+      windEL.text("Wind Speed: " + wind);
+      humidtyEL.text("Humidity: " + humidity);
+      VCDEL.text("UV Index: " + uVIndex);
+    });
+}
+// UV INDEX 2nd API CALL
+function uv(lat, lon) {
+  var uvURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&appid=1e41fe8715bf0efe92d1f293a98c2c2d8&exclude=minutely,hourly";
+  fetch(uvURL)
+    .then(function (response) {
+      //GET responses
+      return response.json();
+    })
+    .then(function (data) {
+      uVIndex = data.current.uvi;
+      fiveDays(data.daily);
+    });
+}
+
+console.log("WORKING!");
